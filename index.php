99c474e3f5fab8a6c0c7e23c923f362f3736fd9e
Matthias <mhaasler@web.de>
2018-01-28 14:59:27 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Homeserver</title>
+
+    <!-- Custom styles for this template -->
+    <link href="css/main.css" rel="stylesheet">
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+<nav class="navbar navbar-inverse">
+    <!--    <div class="container">-->
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+                aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Homeserver</a>
+    </div>
+    <div id="navbar" class="collapse navbar-collapse">
+        <ul class="nav navbar-nav">
+            <li><a href="/welcome/wiki/">Wiki</a></li>
+            <li><a href="http://homeserver.com">Collector</a></li>
+        </ul>
+    </div><!--/.nav-collapse -->
+    <!--    </div>-->
+</nav>
+
+<div class="container">
+
+    <div class="page-header">
+        <h1>Welcome to my Homeserver</h1>
+        <p class="lead">
+            You can either go to the wiki or to the invoice App.
+        </p>
+    </div>
+    <div class="row marketing">
+        <div class="col-md-6">
+            <div class="kachel"><a href="http://homeserver.com">
+                    <img src="http://placehold.it/80x80" class="img-circle">
+                </a>
+                <h2>Collector</h2>
+                <p>My Invoice App. It is only open to me - sorry</p></div>
+        </div>
+        <div class="col-md-6">
+            <div class="kachel">
+                <a href="http://wiki.homeserver.com">
+                    <img src="img/dokuwiki-128.png" class="img-circle">
+                </a>
+                <h2>Wiki
+                </h2>
+                <p>My Wiki. It is only open to by me registered users - sorry</p></div>
+        </div>
+
+    </div>
+
+</div><!-- /.container -->
+
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
